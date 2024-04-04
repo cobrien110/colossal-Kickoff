d339bb2f95ab2a2446988c5e5ed0d593a5715e94
WingedCobold <85312857+WingedCobold@users.noreply.github.com>
2024-04-04 19:48:42 -0400

    Ball physics, sound effects part 1

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource source;
+    public AudioClip[] sounds;
+    public float volume = 1f;
+    public float pitchRangeLow = .85f;
+    public float pitchRangeHigh = 1.15f;
+
+    private void Start()
+    {
+        if (source == null)
+        {
+            source = GetComponent<AudioSource>();
+        }
+    }
+
+    public void PlaySound(AudioClip sound)
+    {
+        if (sound == null) return;
+        source.clip = sound;
+        source.volume = volume;
+        source.Play();
+        Debug.Log("Playing sound: " + sound.name);
+    }
+
+    public void PlaySoundRandomPitch(AudioClip sound)
+    {
+        source.pitch = Random.Range(pitchRangeLow, pitchRangeHigh);
+        PlaySound(sound);
+    }
+
+    public void PlaySoundSpecificPitch(AudioClip sound, float pitch)
+    {
+        source.pitch = pitch;
+        PlaySound(sound);
+    }
+
+    public AudioClip Find(string soundName)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == soundName)
+            {
+                return sounds[i];
+            }
+        }
+        return null;
+    }
+
+}
--- Assets/Scripts/BallProperties.cs
+++ Assets/Scripts/BallProperties.cs
@@ -7,12 +7,14 @@ public class BallProperties : MonoBehaviour
     [HideInInspector] public GameObject ballOwner = null;
     private UIManager UM = null;
     private GameplayManager GM = null;
+    private AudioPlayer audioPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         UM = GameObject.Find("Canvas").GetComponent<UIManager>();
         GM = GameObject.Find("Gameplay Manager").GetComponent<GameplayManager>();
+        audioPlayer = GetComponent<AudioPlayer>();
     }
 
     // Update is called once per frame
@@ -40,6 +42,15 @@ public class BallProperties : MonoBehaviour
             UM.warriorPoint();
             GM.Reset();
             Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+        {
+            string bouncePick = Random.Range(1, 3).ToString();
+            audioPlayer.PlaySoundRandomPitch(audioPlayer.Find("bounce" + bouncePick));
         }
     }
 }
