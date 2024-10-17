c47ffa691c5a8e1da85a0ee0bcf082baff1a0bb1
WingedCobold <89876727+cobrien110@users.noreply.github.com>
2024-10-17 15:38:18 -0400

    turned Canvas into a prefab

--- Assets/Scripts/SnakeSegment.cs
+++ Assets/Scripts/SnakeSegment.cs
@@ -7,11 +7,13 @@ public class SnakeSegment : MonoBehaviour
     //public int numBeforeSplit = 4;
     public AbilitySnakeSegments SS;
     public int index = -1;
+    public GameplayManager GM;
 
     // Start is called before the first frame update
     void Start()
     {
         SS = GameObject.FindGameObjectWithTag("Monster").GetComponent<AbilitySnakeSegments>();
+        GM = GameObject.Find("Gameplay Manager").GetComponent<GameplayManager>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class SnakeSegment : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (GM == null || !GM.isPlaying) return;
         if (SS.minNumOfSegmentsToSplit >= SS.segments.Count) return;
         if (other.CompareTag("Monster"))
         {
