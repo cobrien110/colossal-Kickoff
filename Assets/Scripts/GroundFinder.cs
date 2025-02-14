e045f67a7685362b59506218d5e8c2128f22f3f8
WingedCobold <85312857+WingedCobold@users.noreply.github.com>
2025-02-13 20:05:14 -0500

    Reorganized Files + shadows will always be on ground level

---
+++ Assets/Scripts/GroundFinder.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundFinder : MonoBehaviour
+{
+    public LayerMask groundLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, groundLayer))
+        {
+            transform.position = new Vector3(transform.position.x, hit.point.y + 0.01f, transform.position.z);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
