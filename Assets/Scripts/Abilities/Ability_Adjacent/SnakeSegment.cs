ca56dfcdedf08932516d5098602e8ad6d211b34a
WingedCobold <85312857+WingedCobold@users.noreply.github.com>
2025-02-18 20:29:05 -0500

    quetz wing segments + passive fix

--- Assets/Scripts/Abilities/Ability_Adjacent/SnakeSegment.cs
+++ Assets/Scripts/Abilities/Ability_Adjacent/SnakeSegment.cs
@@ -8,18 +8,21 @@ public class SnakeSegment : MonoBehaviour
     public AbilitySnakeSegments SS;
     public int index = -1;
     public GameplayManager GM;
+    public Sprite spr1;
+    public Sprite spr2;
+    private SpriteRenderer SR;
 
     // Start is called before the first frame update
     void Start()
     {
         SS = GameObject.FindGameObjectWithTag("Monster").GetComponent<AbilitySnakeSegments>();
         GM = GameObject.Find("Gameplay Manager").GetComponent<GameplayManager>();
-    }
+        SR = GetComponentInChildren<SpriteRenderer>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if ((index + 3) % 4 == 0)
+        {
+            SR.sprite = spr2;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
