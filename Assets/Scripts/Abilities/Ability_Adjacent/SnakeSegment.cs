328c0747df3676c795c1eb44923c77772ee40f71
WingedCobold <89876727+cobrien110@users.noreply.github.com>
2025-02-28 17:36:02 -0500

    quetz wing flaps + removed colliders on podium

--- Assets/Scripts/Abilities/Ability_Adjacent/SnakeSegment.cs
+++ Assets/Scripts/Abilities/Ability_Adjacent/SnakeSegment.cs
@@ -8,20 +8,24 @@ public class SnakeSegment : MonoBehaviour
     public AbilitySnakeSegments SS;
     public int index = -1;
     public GameplayManager GM;
-    public Sprite spr1;
-    public Sprite spr2;
-    private SpriteRenderer SR;
+    public Animator ANIM;
+    //private SpriteRenderer SR;
 
     // Start is called before the first frame update
     void Start()
     {
         SS = GameObject.FindGameObjectWithTag("Monster").GetComponent<AbilitySnakeSegments>();
         GM = GameObject.Find("Gameplay Manager").GetComponent<GameplayManager>();
-        SR = GetComponentInChildren<SpriteRenderer>();
+        //SR = GetComponentInChildren<SpriteRenderer>();
+        ANIM = GetComponentInChildren<Animator>();
 
+        if (ANIM == null) return;
         if ((index + 3) % 4 == 0)
         {
-            SR.sprite = spr2;
+            ANIM.SetBool("isWing", true);
+        } else
+        {
+            ANIM.SetBool("isWing", false);
         }
     }
 
