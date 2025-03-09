24a46ddb45772613dcf90a726af4df3a2e72264e
WingedCobold <89876727+cobrien110@users.noreply.github.com>
2025-03-09 18:04:47 -0400

    cutscene fade in and out, animation synced to talking

--- Assets/Scripts/CutsceneManager.cs
+++ Assets/Scripts/CutsceneManager.cs
@@ -5,10 +5,11 @@ using TMPro;
 using System;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class CutsceneManager : MonoBehaviour
 {
-    public Sprite SPR;
+    public Animator ANIM;
     public TextMeshProUGUI subtitlesText;
     public TextMeshProUGUI lineNumText;
     private int lineNum;
@@ -16,6 +17,13 @@ public class CutsceneManager : MonoBehaviour
     public GameObject subtitles;
     public String menuSceneName;
     public AudioPlayer sound;
+    public Image fadeIn;
+    public float fadeSpeed = 1f;
+    bool isFadingIn = false;
+
+    // sound management
+    private float[] samples = new float[1024]; // Buffer for sample data
+    private float silenceThreshold = 0.01f; // Adjust this threshold as needed
 
     [Serializable]
     public struct VoiceLine
@@ -31,13 +39,40 @@ public class CutsceneManager : MonoBehaviour
     {
         subtitlesText.text = "";
         lineNumText.text = "";
+        subtitlesText.color = Color.green;
+
+        ANIM.SetBool("isTalking", false);
+
         //subtitlesText.text = "TEST TEST";
         StartCoroutine(StartSubtitles());
     }
 
     void Update()
     {
-        var gamepad = Gamepad.current;
+        // fade in and out
+        if (isFadingIn)
+        {
+            float a = fadeIn.color.a + fadeSpeed * Time.deltaTime;
+            a = Mathf.Clamp(a, 0, 1);
+            fadeIn.color = new Color(0, 0, 0, a);
+        } else
+        {
+            float a = fadeIn.color.a - fadeSpeed * Time.deltaTime;
+            a = Mathf.Clamp(a, 0, 1);
+            fadeIn.color = new Color(0, 0, 0, a);
+        }
+
+        // close mouth when not talking
+        if (sound.isPlaying())
+        {
+            sound.source.clip.GetData(samples, sound.source.timeSamples);
+            float maxAmplitude = GetMaxAmplitude(samples);
+
+            bool willTalk = maxAmplitude > silenceThreshold ? true : false;
+            ANIM.SetBool("isTalking", willTalk);
+        }
+
+            var gamepad = Gamepad.current;
         if (gamepad != null)
         {
             if (gamepad.startButton.wasPressedThisFrame)
@@ -82,12 +117,28 @@ public class CutsceneManager : MonoBehaviour
 
     void DoEvent(int eventNum)
     {
-        if (eventNum == 3)
+        if (eventNum == 0)
         {
-            //subtitlesText.color = Color.red;
-        } else
-        {
-            subtitlesText.color = Color.green;
+            ANIM.SetBool("isTalking", true);
         }
+        if (eventNum == 13)
+        {
+            //ANIM.Play("OrcmanSmash");
+        }
+        if (eventNum == 37)
+        {
+            isFadingIn = true;
+        } 
+    }
+
+    private float GetMaxAmplitude(float[] data)
+    {
+        float max = 0f;
+        foreach (float sample in data)
+        {
+            if (Mathf.Abs(sample) > max)
+                max = Mathf.Abs(sample);
+        }
+        return max;
     }
 }
