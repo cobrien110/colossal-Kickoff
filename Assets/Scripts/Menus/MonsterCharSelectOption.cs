6f8e237281f2a3d0cf9073850f8033344b3ac074
WingedCobold <89876727+cobrien110@users.noreply.github.com>
2025-03-10 18:47:15 -0400

    menu cursor fix

--- Assets/Scripts/Menus/MonsterCharSelectOption.cs
+++ Assets/Scripts/Menus/MonsterCharSelectOption.cs
@@ -18,6 +18,7 @@ public class MonsterCharSelectOption: MonoBehaviour
         sizeBC = BC.size;
         image.sprite = monsterSprites[0];
         image.transform.localScale = new Vector3(monsterScales[0], monsterScales[0], 1f);
+        BC.size = new Vector2(sizeBC.x / monsterScales[0], sizeBC.y / monsterScales[0]);
         //image.transform.position = monsterPositions[0];
     }
 
