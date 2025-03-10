1f32e3f34dd8bd032cabe45e2e227659d1956fdc
WingedCobold <89876727+cobrien110@users.noreply.github.com>
2025-03-09 21:36:28 -0400

    main menu touch-ups

--- Assets/Scripts/Menus/MonsterCharSelectOption.cs
+++ Assets/Scripts/Menus/MonsterCharSelectOption.cs
@@ -6,12 +6,25 @@ using UnityEngine.UI;
 public class MonsterCharSelectOption: MonoBehaviour
 {
     [SerializeField] private Sprite[] monsterSprites;
+    [SerializeField] private float[] monsterScales;
+    [SerializeField] private Vector3[] monsterPositions;
+    [SerializeField] private Image image;
+    private BoxCollider2D BC;
+    private Vector2 sizeBC;
 
     void Start() {
-        GetComponent<Image>().sprite = monsterSprites[0];
+        image = GetComponent<Image>();
+        BC = GetComponent<BoxCollider2D>();
+        sizeBC = BC.size;
+        image.sprite = monsterSprites[0];
+        image.transform.localScale = new Vector3(monsterScales[0], monsterScales[0], 1f);
+        //image.transform.position = monsterPositions[0];
     }
 
     public void updateSprite(int index) {
-        GetComponent<Image>().sprite = monsterSprites[index];
+        image.sprite = monsterSprites[index];
+        image.transform.localScale = new Vector3(monsterScales[index], monsterScales[index], 1f);
+        BC.size = new Vector2(sizeBC.x / monsterScales[index], sizeBC.y / monsterScales[index]);
+        //image.transform.localPosition = monsterPositions[index];
     }
 }
