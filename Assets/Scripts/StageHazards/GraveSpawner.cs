c1807b27aee858289ecb7a3fd5181a8ee163b53c
HazardLaser <59776482+HazardLaser@users.noreply.github.com>
2025-04-08 19:33:52 -0400

    tweak japan

---
+++ Assets/Scripts/StageHazards/GraveSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraveSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] group1;
+
+    [SerializeField] private GameObject[] gravePrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnGameStart()
+    {
+
+    }
+
+    public void OnRoundStart()
+    {
+
+    }
+}
