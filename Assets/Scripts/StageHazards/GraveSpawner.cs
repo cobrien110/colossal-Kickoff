158a4de8aaa328a2dd7fbce4d62c15db69fc3ea4
HazardLaser <59776482+HazardLaser@users.noreply.github.com>
2025-04-08 20:38:54 -0400

    Grave stage hazard rework

--- Assets/Scripts/StageHazards/GraveSpawner.cs
+++ Assets/Scripts/StageHazards/GraveSpawner.cs
@@ -7,25 +7,55 @@ public class GraveSpawner : MonoBehaviour
     [SerializeField] private Transform[] group1;
 
     [SerializeField] private GameObject[] gravePrefabs;
-    // Start is called before the first frame update
+
+    [SerializeField] private GameplayManager GM = null;
+
+    private List<GraveHazard> spawnedGraves = new List<GraveHazard>();
+    private bool wasPlayingLastFrame = false;
+
     void Start()
     {
-        
+        OnGameStart();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (GM == null) return;
+
+        bool isPlaying = GM.IsPlayingGet();
+
+        //Detect round end (isPlaying becomes false)
+        if (wasPlayingLastFrame && !isPlaying)
+        {
+            //Called at end of round, before next begins
+            OnRoundStart(); 
+        }
+
+        wasPlayingLastFrame = isPlaying;
     }
 
     public void OnGameStart()
     {
+        //Place one random grave at each group1 point, keep them in memory
 
+        foreach (Transform point in group1)
+        {
+            GameObject prefab = gravePrefabs[Random.Range(0, gravePrefabs.Length)];
+            GameObject grave = Instantiate(prefab, point.position, point.rotation, point);
+            spawnedGraves.Add(grave.GetComponent<GraveHazard>());
+        }
     }
 
     public void OnRoundStart()
     {
+        //Reset any state on graves here (if needed), but don’t destroy them
 
+        foreach (GraveHazard grave in spawnedGraves)
+        {
+            if (grave != null)
+            {
+                grave.ResetGrave();
+            }
+        }
     }
 }
