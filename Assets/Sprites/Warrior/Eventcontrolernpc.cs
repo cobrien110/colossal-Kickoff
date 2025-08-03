53fd5517af92cb4188b45fa1a76683dd1b2186dd
HazardLaser <59776482+HazardLaser@users.noreply.github.com>
2025-08-03 15:55:08 -0400

    Updated camera position

---
+++ Assets/Sprites/Warrior/Eventcontrolernpc.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eventcontrolernpc : MonoBehaviour
+{
+    [SerializeField] int[] eventArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
