59aa6b9234d9d8ea933178ad87f48d29f6aeb689
HazardLaser <59776482+HazardLaser@users.noreply.github.com>
2025-08-07 18:42:46 -0400

    Physcial monsters and players

--- Assets/Scripts/Menus/CharacterSelectOption.cs
+++ Assets/Scripts/Menus/CharacterSelectOption.cs
@@ -6,35 +6,38 @@ using UnityEngine.UI;
 public class CharacterSelectOption : MonoBehaviour
 {
     [SerializeField] public int characterID;
-    //number of cursors currently hovering over this menu option
     [SerializeField] private int cursorsOver = 0;
-    [SerializeField] private UIDropShadow shadow;
+    [SerializeField] private GameObject highlightObject;
 
     public bool canBeSelected = true;
 
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag == "MenuCursor")
+    void Start()
+    {
+        if (highlightObject != null)
+            highlightObject.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("MenuCursor"))
         {
-            //other.gameObject.GetComponent<MenuCursor>().setSelected(characterID);
-            other.gameObject.GetComponent<MenuCursor>().StartHovering("playerSelect", characterID);
+            other.GetComponent<MenuCursor>().StartHovering("playerSelect", characterID);
             cursorsOver++;
-            shadow.enabled = true;
+
+            if (highlightObject != null)
+                highlightObject.SetActive(true);
         }
     }
 
-    void OnTriggerExit2D(Collider2D other) {
-        if (canBeSelected)
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (canBeSelected && other.CompareTag("MenuCursor"))
         {
-            if (other.gameObject.tag == "MenuCursor")
-            {
-                other.gameObject.GetComponent<MenuCursor>().StopHovering();
-                cursorsOver--;
-                if (cursorsOver <= 0)
-                {
-                    shadow.enabled = false;
-                }
-            }
+            other.GetComponent<MenuCursor>().StopHovering();
+            cursorsOver--;
+
+            if (cursorsOver <= 0 && highlightObject != null)
+                highlightObject.SetActive(false);
         }
     }
-    
 }
--- Assets/Scripts/Menus/MonsterCharSelectOption.cs
+++ Assets/Scripts/Menus/MonsterCharSelectOption.cs
@@ -5,27 +5,34 @@ using UnityEngine.UI;
 
 public class MonsterCharSelectOption: MonoBehaviour
 {
-    [SerializeField] private Sprite[] monsterSprites;
-    [SerializeField] private float[] monsterScales;
-    [SerializeField] private Vector3[] monsterPositions;
-    [SerializeField] private Image image;
-    private BoxCollider2D BC;
-    private Vector2 sizeBC;
+    [SerializeField] private GameObject[] monsterObjects;
+    private int currentIndex = 0;
 
-    void Start() {
-        image = GetComponent<Image>();
-        BC = GetComponent<BoxCollider2D>();
-        sizeBC = BC.size;
-        image.sprite = monsterSprites[0];
-        image.transform.localScale = new Vector3(monsterScales[0], monsterScales[0], 1f);
-        BC.size = new Vector2(sizeBC.x / monsterScales[0], sizeBC.y / monsterScales[0]);
-        //image.transform.position = monsterPositions[0];
+    void Start()
+    {
+        UpdateMonster(currentIndex);
     }
 
-    public void updateSprite(int index) {
-        image.sprite = monsterSprites[index];
-        image.transform.localScale = new Vector3(monsterScales[index], monsterScales[index], 1f);
-        BC.size = new Vector2(sizeBC.x / monsterScales[index], sizeBC.y / monsterScales[index]);
-        //image.transform.localPosition = monsterPositions[index];
+    public void UpdateMonster(int index)
+    {
+        if (index < 0 || index >= monsterObjects.Length)
+        {
+            Debug.LogWarning("Monster index out of range.");
+            return;
+        }
+
+        //Disable all monsters
+        for (int i = 0; i < monsterObjects.Length; i++)
+        {
+            if (monsterObjects[i] != null)
+                monsterObjects[i].SetActive(false);
+        }
+
+        //Enable the selected monster
+        if (monsterObjects[index] != null)
+        {
+            monsterObjects[index].SetActive(true);
+            currentIndex = index;
+        }
     }
 }
