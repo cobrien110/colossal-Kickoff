8202ff238d01465c1f37b35c82d4823e0a79001b
Jackv4848 <jackv4848@gmail.com>
2025-09-30 13:57:14 -0400

    Hovering overlap issue fix

--- Assets/Scripts/Menus/MonsterCharSelectOption.cs
+++ Assets/Scripts/Menus/MonsterCharSelectOption.cs
@@ -7,6 +7,7 @@ public class MonsterCharSelectOption: MonoBehaviour
 {
     [SerializeField] private Sprite[] monsterSprites;
     [SerializeField] private float[] monsterScales;
+    [SerializeField] private float[] monsterOffsets;
     [SerializeField] private Vector3[] monsterPositions;
     [SerializeField] private Image image;
     private BoxCollider2D BC;
@@ -18,14 +19,16 @@ public class MonsterCharSelectOption: MonoBehaviour
         sizeBC = BC.size;
         image.sprite = monsterSprites[0];
         image.transform.localScale = new Vector3(monsterScales[0], monsterScales[0], 1f);
-        BC.size = new Vector2(sizeBC.x / monsterScales[0], sizeBC.y / monsterScales[0]);
+        BC.size = new Vector2(sizeBC.x / (monsterScales[0] * 1.75f), sizeBC.y / (monsterScales[0] * 1.25f));
+        BC.offset = new Vector2(monsterOffsets[0], -3.0f);
         //image.transform.position = monsterPositions[0];
     }
 
     public void updateSprite(int index) {
         image.sprite = monsterSprites[index];
         image.transform.localScale = new Vector3(monsterScales[index], monsterScales[index], 1f);
-        BC.size = new Vector2(sizeBC.x / monsterScales[index], sizeBC.y / monsterScales[index]);
+        BC.size = new Vector2(sizeBC.x / (monsterScales[index] * 1.75f), sizeBC.y / (monsterScales[index] * 1.50f));
+        BC.offset = new Vector2(monsterOffsets[index], -3.0f);
         //image.transform.localPosition = monsterPositions[index];
     }
 }
